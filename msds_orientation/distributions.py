6023c1d1d1263a5016e6a5d88ef934fd3f550dca
hvalenty <hvalenty26@gmail.com>
2024-06-17 00:08:29 -0400

    Distribution updates

--- msds_orientation/distributions.py
+++ msds_orientation/distributions.py
@@ -4,3 +4,11 @@ import numpy as np
 data = [np.random.randn(10000)]
 plt.hist(data)
 plt.show()
+
+data = [np.random.binomial(9, 0.1, 20000)]
+plt.hist(data)
+plt.show()
+
+data = [np.random.uniform(50, 150, 10000)]
+plt.hist(data)
+plt.show()
